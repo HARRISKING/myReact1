e076d43cb161f0e467227c93c9e954a7bfd1e6ff
HARRISKING <llc13588440410@outlook.com>
2017-04-18 11:04:55 +0800

    yasuoban

--- app/index.js
+++ app/index.js
@@ -1,17 +1,15 @@
-// import _ from 'lodash'
-// import $ from 'jquery'
-// import har from "./har"
-//
-// function component () {
-//   var element = $('<div></div>');
-//   /* lodash is required for the next line to work */
-//   element.html(_.join(['Hars -p1','webpack'], ' '));
-//
-//   return element.get(0);
-// }
-//
-// document.body.appendChild(component());
-// console.log(har);
-// console.log(har());
+import _ from 'lodash'
+import $ from 'jquery'
+import har from "./har"
 
-console.log(1);
+function component () {
+  var element = $('<div></div>');
+  /* lodash is required for the next line to work */
+  element.html(_.join(['Hars -p2','webpack'], ' '));
+
+  return element.get(0);
+}
+
+document.body.appendChild(component());
+console.log(har);
+console.log(har());
