31e79f62f359b4c73b43819eb920038a4d3616b6
HARRISKING <llc13588440410@outlook.com>
2017-04-18 10:45:55 +0800

    4.18

--- app/index.js
+++ app/index.js
@@ -1,12 +1,15 @@
 import _ from 'lodash'
 import $ from 'jquery'
+import har from "./har"
 
 function component () {
   var element = $('<div></div>');
   /* lodash is required for the next line to work */
-  element.html(_.join(['Hsssdde','webpack'], ' '));
+  element.html(_.join(['Hars -p1','webpack'], ' '));
 
   return element.get(0);
 }
 
 document.body.appendChild(component());
+console.log(har);
+console.log(har());
