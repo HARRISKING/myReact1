655d833f2cc2f546c164ff9f18c707e3ca17fea2
HARRISKING <llc13588440410@outlook.com>
2017-04-24 09:23:47 +0800

    4.23

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,8 +1,9 @@
 import React from 'react';
-//得到react框架的支持！
+//定义Welcome作为React.Component的子类，因此要引用react。
 
 // 方法一：
 class Welcome extends React.Component{
+  // 定义一个类名叫Welcome,这个类作为React.Component的子类
   constructor(props){
     super(props)
     this.state = {
@@ -14,9 +15,6 @@ class Welcome extends React.Component{
       })
     })
   }
-
-
-
   render(){
     // return <h1>Hello,Component</h1>;
     // return <h1>Hello,{this.props.name}</h1>;
@@ -38,3 +36,4 @@ class Welcome extends React.Component{
 
 export default Welcome
 // export default 用来导出一个值或者需要模型的一个返回值，这里导出Welcome的返回值；
+// 这里是将子类Welcome导出给index.js引用
