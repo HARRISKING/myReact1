b95029a3ea22062f2ffb06c39835b5720da6e28c
HARRISKING <harrisking@txkjdeiMac.local>
2017-07-20 17:32:02 +0800

    监听回车函数

--- react-app1.1/src/App.js
+++ react-app1.1/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import TodoInput from './TodoInput';
+import TodoItem from './TodoItem';
+import 'normalize.css';
+import './reset.css';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      newTodo: "test",
+      newTodo: "",
       todoList: [
         {id:1,title:'第一个代办'},
         {id:2,title:'第二个代办'}
@@ -17,7 +21,7 @@ class App extends Component {
 
   render() {
     let todos = this.state.todoList.map((item,index)=>{
-      return <li>{item.title}</li>
+      return <TodoItem todo={item} />
     })
 
 
@@ -25,7 +29,7 @@ class App extends Component {
       <div className="App">
         <h1>事项清单</h1>
         <div className='inputWrapper'>
-          <input type='text' value={this.state.newTodo} />
+          <TodoInput content={this.state.newTodo} />
         </div>
         <ol>
           {todos}
