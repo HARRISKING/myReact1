b71de2312f9e93a743f7bbf47c50293dd89a112a
HARRISKING <harrisking@txkjdeiMac.local>
2017-07-21 10:57:52 +0800

    第二遍搞定，大概真的了解一些啦

--- react-app1.1/src/App.js
+++ react-app1.1/src/App.js
@@ -17,11 +17,14 @@ class App extends Component {
     }
   }
 
-
-
   render() {
-    let todos = this.state.todoList.map((item,index)=>{
-      return <TodoItem todo={item} />
+    let todos = this.state.todoList.filter((item)=>!item.deleted).map((item,index)=>{
+      return (
+        <li key={index}> 
+          <TodoItem todo={item} onToggle={this.toggle.bind(this)}
+          onDelete={this.delete.bind(this)}/>      
+        </li>
+      )
     })
 
 
@@ -29,14 +32,48 @@ class App extends Component {
       <div className="App">
         <h1>事项清单</h1>
         <div className='inputWrapper'>
-          <TodoInput content={this.state.newTodo} />
+          <TodoInput content={this.state.newTodo} 
+          onSubmit={this.addTodo.bind(this)}
+          onChange={this.changeTitle.bind(this)} />
         </div>
-        <ol>
+        <ol className='todoList'>
           {todos}
         </ol>
       </div>
     );
   }
+  delete(event,todo){
+    todo.deleted = true;
+    this.setState(this.state)
+  }
+  toggle(e,todo){
+    todo.status = todo.status === 'completed' ? '' : 'completed'
+    this.setState(this.state)
+  }
+  changeTitle(event){
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
+  }
+  addTodo(event){
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
+  }
 }
 
 export default App;
+
+let id = 0;
+function idMaker(){
+  id += 1;
+  return id;
+}
